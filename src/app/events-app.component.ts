81539cbbf0af7ac81b189ca8b7d3bad6807ceeec
venky <=>
2019-09-14 18:29:45 +0530

    first commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<h1>Hello Wordl! </h1>
+  <img src="/assets/images/basic-shield.png"/>`
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
