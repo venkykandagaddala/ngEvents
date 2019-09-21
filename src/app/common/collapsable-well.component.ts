d71770a16f034d0006c0e1928ea9b52e694fabb6
venky <=>
2019-09-21 16:35:31 +0530

    Multiple slot content projection

--- src/app/common/collapsable-well.component.ts
+++ src/app/common/collapsable-well.component.ts
@@ -3,8 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   template: `
     <div (click)='toggleContent()' class="well pointable">
-      <h4 class='well-title'> {{title}} </h4>
-      <ng-content *ngIf='visible'></ng-content>
+      <h4><ng-content select="[well-title]"></ng-content>
+       </h4>
+      <ng-content *ngIf='visible' select='[well-body]'></ng-content>
     </div>
   `,
   selector: 'collapsable-well'
