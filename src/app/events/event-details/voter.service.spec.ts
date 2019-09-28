221723fcf3244430057f199f96b5d301bbf79496
venky <=>
2019-09-29 01:00:59 +0530

    Testing Mock calls - Voter service

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -21,6 +21,14 @@ describe('VoterService', () => {
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toEqual('venky');
     });
+
+    it('should call http.delete with right url', () => {
+      const session = {id: 3, voters: ['venky', 'kkkk']};
+      mockHttp.delete.and.returnValue(of(false));
+      voterService.deleteVoter(6, <ISession>session, 'kkkk');
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/6/sessions/3/voters/kkkk');
+
+    });
   });
 
   describe('addVoter', () => {
@@ -30,6 +38,14 @@ describe('VoterService', () => {
       voterService.addVoter(3, <ISession>session, 'New User');
       expect(session.voters.length).toBe(3);
       expect(session.voters.includes('New User')).toBe(true);
+    });
+
+    it('should call http.post with right url', () => {
+      const session = {id: 3, voters: ['venky', 'kkkk']};
+      mockHttp.post.and.returnValue(of(false));
+      voterService.addVoter(3, <ISession>session, 'new-user');
+      expect(mockHttp.post)
+        .toHaveBeenCalledWith('/api/events/3/sessions/3/voters/new-user', {}, jasmine.any(Object));
     });
   });
 });
